cb9348aa7523af3fe68abbda9fa9b1b48461608c
Monika Widurska <monikawidurska@op.pl>
2021-12-23 17:08:11 +0100

    Wykonanie zadania 3

--- cwiczenie1/Program.cs
+++ cwiczenie1/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace cwiczenie1
+namespace Zad3Kalkulator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Aplikacja kalkulator tylko mnożenie!");
+
+            Console.WriteLine("Podaj proszę 1 liczbę:");
+
+            var liczba1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj proszę 2 liczbę:");
+
+            var liczba2 = int.Parse(Console.ReadLine());
+
+            var wynik = liczba1 * liczba2;
+
+            Console.WriteLine($"Wynik Twojego działania to: {wynik}.");
         }
     }
 }
