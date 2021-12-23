cd51cada2a63a6a3f1992aec74941f8119bd73d8
Monika Widurska <monikawidurska@op.pl>
2021-12-23 16:53:26 +0100

    Wykonanie zadania 2

--- cwiczenie1/Program.cs
+++ cwiczenie1/Program.cs
@@ -6,7 +6,25 @@ namespace cwiczenie1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int iloczyn = 1;
+
+            for (int licznik = 1; licznik <= 9; licznik ++)  
+            {
+                iloczyn = iloczyn * licznik; 
+                Console.WriteLine(" silnia liczby " + licznik + " jest równa " + iloczyn);
+            }
+            iloczyn = 1;
+            for (int licznik = 1; licznik <= 20; licznik++)
+            {
+                iloczyn = iloczyn * licznik;
+                Console.WriteLine(" silnia liczby " + licznik + " jest równa " + iloczyn);
+            }
+            iloczyn = 1;
+            for (int licznik = 1; licznik <= 243; licznik++)
+            {
+                iloczyn = iloczyn * licznik;
+                Console.WriteLine(" silnia liczby " + licznik + " jest równa " + iloczyn);
+            }
         }
     }
 }
