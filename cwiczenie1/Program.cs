eb0cf8d321cf5f8fed8838fd868f1ded47559a68
Monika Widurska <monikawidurska@op.pl>
2021-12-23 17:44:42 +0100

    Wykonanie zadania 3c

--- cwiczenie1/Program.cs
+++ cwiczenie1/Program.cs
@@ -6,32 +6,46 @@ namespace Zad3Kalkulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Aplikacja kalkulator tylko mnożenie!");
-
-            Console.WriteLine("Podaj proszę 1 liczbę:");
-
-            var liczba1 = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Podaj proszę 2 liczbę:");
-
-            var liczba2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Wpisz '+' , '-' , '*'");
-            var operacja = Console.ReadLine();
-            var wynik = 0;
-            switch (operacja)
+            try
             {
-                case "*": wynik = liczba1 * liczba2;   
-                    break;
-                case "+":  wynik = liczba1 + liczba2; 
-                    break;
-                case "-":  wynik = liczba1 - liczba2; 
-                    break;
-                default: Console.WriteLine("Zły znak!");
-                    break;
+                Console.WriteLine("Aplikacja kalkulator tylko mnożenie!");
 
+                Console.WriteLine("Podaj proszę 1 liczbę:");
+
+                var liczba1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Podaj proszę 2 liczbę:");
+
+                var liczba2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Wpisz '+' , '-' , '*', '/'");
+                var operacja = Console.ReadLine();
+                var wynik = 0;
+                switch (operacja)
+                {
+                    case "*":
+                        wynik = liczba1 * liczba2;
+                        break;
+                    case "+":
+                        wynik = liczba1 + liczba2;
+                        break;
+                    case "-":
+                        wynik = liczba1 - liczba2;
+                        break;
+                    case "/":
+                        wynik = liczba1 / liczba2;
+                        break;
+                    default:
+                        Console.WriteLine("Zły znak!");
+                        break;
+
+                }
+
+                Console.WriteLine($"Wynik Twojego działania to: {wynik}.");
             }
-
-            Console.WriteLine($"Wynik Twojego działania to: {wynik}.");
+            catch (Exception exception) 
+            {
+                Console.WriteLine("Nie dziel przez 0");
+            }
         }
     }
 }
